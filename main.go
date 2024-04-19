fbcf369aeba4a9ab3633a413db2cbc86fc9aea13
ian <ian@user111.ru>
2024-04-19 01:03:12 +0000

    edit data

--- main.go
+++ main.go
@@ -12,23 +12,34 @@ type page struct {
 	Data  []byte
 }
 
-func getPage(title string) *page {
+func getPage(title string) (*page, error) {
 	file := title + ".txt"
 	data, _ := os.ReadFile(file)
 	return &page{
 		Title: title,
 		Data:  data,
-	}
+	}, nil
 }
 
 func handlerRead(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/read/"):]
-	p := getPage(title)
-	t, _ := template.ParseFiles("template.html")
+	p, _ := getPage(title)
+	t, _ := template.ParseFiles("read.html")
+	t.Execute(w, p)
+}
+
+func handlerEdit(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := getPage(title)
+	if err != nil {
+		p = &page{Title: title, Data: []byte{}}
+	}
+	t, _ := template.ParseFiles("edit.html")
 	t.Execute(w, p)
 }
 
 func main() {
 	http.HandleFunc("/read/", handlerRead)
+	http.HandleFunc("/edit/", handlerEdit)
 	log.Fatal(http.ListenAndServe(":8089", nil))
 }
