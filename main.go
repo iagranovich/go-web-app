a18b21878b502c283c30890180d9571c73970385
ian <ian@user111.ru>
2024-04-19 21:52:10 +0000

    save data

--- main.go
+++ main.go
@@ -12,9 +12,17 @@ type page struct {
 	Data  []byte
 }
 
+func (p *page) save() error {
+	filename := p.Title + ".txt"
+	return os.WriteFile(filename, p.Data, 0600)
+}
+
 func getPage(title string) (*page, error) {
 	file := title + ".txt"
-	data, _ := os.ReadFile(file)
+	data, err := os.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
 	return &page{
 		Title: title,
 		Data:  data,
@@ -24,6 +32,7 @@ func getPage(title string) (*page, error) {
 func handlerRead(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/read/"):]
 	p, _ := getPage(title)
+
 	t, _ := template.ParseFiles("read.html")
 	t.Execute(w, p)
 }
@@ -32,14 +41,24 @@ func handlerEdit(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/edit/"):]
 	p, err := getPage(title)
 	if err != nil {
-		p = &page{Title: title, Data: []byte{}}
+		p = &page{Title: title}
 	}
+
 	t, _ := template.ParseFiles("edit.html")
 	t.Execute(w, p)
+}
+
+func handlerSave(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	data := r.FormValue("data")
+	p := &page{Title: title, Data: []byte(data)}
+	p.save()
+	http.Redirect(w, r, "/read/"+title, http.StatusFound)
 }
 
 func main() {
 	http.HandleFunc("/read/", handlerRead)
 	http.HandleFunc("/edit/", handlerEdit)
+	http.HandleFunc("/save/", handlerSave)
 	log.Fatal(http.ListenAndServe(":8089", nil))
 }
